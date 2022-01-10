d2430a7b3a5d9f8ee4a8bac674402d25c5908763
jeong-sik <yousleepwhen@gmail.com>
2022-01-10 14:10:35 +0900

    fix: add test case

--- rescript/src/View.js
+++ rescript/src/View.js
@@ -14,10 +14,13 @@ var header5 = "w-[100%]";
 
 var header6 = "w-[calc(100%-40px)]";
 
+var header7 = "translate-x-[calc(-50%+27px)]";
+
 exports.header = header;
 exports.header2 = header2;
 exports.header3 = header3;
 exports.header4 = header4;
 exports.header5 = header5;
 exports.header6 = header6;
+exports.header7 = header7;
 /* No side effect */
